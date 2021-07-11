57193f1732d191f186a28a0343e7a369e74dcb9e
Aniruddha Banerjee <imanirudh.dev@gmail.com>
2021-07-11 18:15:33 +0530

    Finally fixed the container.yml issue

--- packages/container/src/App.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import MarketingApp from './components/MarketingApp'
-
-export default () => {
-    return <div>
-
-        <MarketingApp />
-        </div>
-}
--- packages/marketing/src/index.js
+++
@@ -1,3 +0,0 @@
-import('./bootstrap')
-
-console.log('Hi Marketing')
