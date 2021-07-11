78e2e4d551da42a4099d21d22367276ae08b4659
Aniruddha Banerjee <imanirudh.dev@gmail.com>
2021-07-11 18:04:46 +0530

    Changed container.yml script

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -3,6 +3,9 @@ import MarketingApp from './components/MarketingApp'
 
 export default () => {
     return <div>
+        <h1>
+            Hello
+        </h1>
         <MarketingApp />
         </div>
 }
