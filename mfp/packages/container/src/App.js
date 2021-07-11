3637c742e6a0e2f5c48e735e03d8207db8da9aba
Aniruddha Banerjee <imanirudh.dev@gmail.com>
2021-07-11 16:44:08 +0530

    Changed text in the app.js

--- mfp/packages/container/src/App.js
+++ mfp/packages/container/src/App.js
@@ -3,7 +3,7 @@ import MarketingApp from './components/MarketingApp'
 
 export default () => {
     return <div>
-        <h1>Hello From Root Container</h1>
+        <h1>Hello </h1>
         <hr/>
         <MarketingApp />
         </div>
